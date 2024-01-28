cf3678e4f2d71126e0538b708e11ac15d4401716
vietthom <thomle0418@gmail.com>
2024-01-27 19:11:17 -0500

    Basic layout for card component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar";
+import SearchCard from "@/components/searchCard";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <h1>Hello World</h1>
+      <SearchCard />
     </div>
   );
 }
